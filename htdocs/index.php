670e41206f24772a7788800b771a26ea384b91dc
Johann DE-RUS <jde-rus@e1r10p16.42.fr>
2016-02-02 12:51:38 +0100

    ajout d'une image en fond et d'une police

---
+++ htdocs/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "utf-8">
+<link rel = "stylesheet" href = "css/style.css">
+</head>
+<body>
+<h1>Camagru</h1>
+<p>Bienvenu sur Camagru le site de retouche photo ultime</p>
+</body>
+</html>
