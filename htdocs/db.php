54cf3f9e7846996e2170b68cf8710be333a95efa
Johann DE-RUS <jde-rus@e1r10p14.42.fr>
2016-02-08 08:07:54 +0100

    db.php

--- htdocs/db.php
+++ htdocs/db.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	if($db = new PDO('mysql:host=localhost;dbname=db_camagru', 'root', '', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION)))
+	if($db = new PDO('mysql:host=localhost;dbname=db_camagru', 'root', 'johann', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION)))
 	{
 		$query = $db->prepare("SET NAMES 'utf8'");
 		$query->execute();
