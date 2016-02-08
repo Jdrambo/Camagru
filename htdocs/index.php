54b38a25277f5bbd851e1107011040a9a6fba828
Johann DE-RUS <jde-rus@e1r10p14.42.fr>
2016-02-08 09:17:42 +0100

    Affichage des  et  sur index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -14,7 +14,19 @@ spl_autoload_register("loadClass");
 <body>
 <div class = "container">
 	<?php include('header.php');?>
-	<p>Bienvenu sur Camagru le site de retouche photo ultime</p>
+	<p>Bienvenue sur Camagru le site de retouche photo ultime</p>
+	<?php
+		if(isset($_SESSION)){
+			foreach ($_SESSION as $key => $value){
+				echo '<p class = "field">$_SESSION[\'' . $key . '\'] => ' . $value . '</p>';
+			}
+		}
+		if(isset($_SERVER)){
+			foreach ($_SERVER as $key => $value){
+				echo '<p class = "field">$_SERVER[\'' . $key . '\'] => ' . $value . '</p>';
+			}
+		}
+	?>
 </div>
 </body>
 </html>
