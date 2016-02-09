ff63cb0c372742c63f33819ae3ac57f331106305
jdrambo <jdrambo@hotmail.fr>
2016-02-09 16:30:07 +0100

    Ajout d'une classe de création de formulaire

---
+++ htdocs/classes/StdForm.php
@@ -0,0 +1,80 @@
+<?php
+class StdForm{
+    private $_method;
+    private $_action;
+    private $_class;
+    private $_id;
+    private $_title;
+    
+    public function __construct($method, $action, $class, $id, $title){
+        $this->setMethod($method);
+        $this->setAction($action);
+        $this->setClass($class);
+        $this->setId($id);
+        $this->setTitle($title);
+    }
+    
+    public function addInputs($inputs = array()){
+        echo '<form method = "'.$this->getMethod().'" action = "'.$this->getAction().'" class = "'.$this->getClass().'" id = "'.$this->getId().'"><h3>'.$this->getTitle().'</h3>';
+        /*
+        0 => tag de l'input (input, button, radio, select...)
+        1 => type de l'input (text, password, date...)
+        2 => name de l'input
+        3 => classe css
+        4 => placeholder pour input
+        */
+        foreach($inputs as $value){
+            if ($value[0] === "input")
+                echo '<input type = "'.$value[1].'" name = "'.$value[2].'" class = "'.$value[3].'" placeholder = "'.$value[4].'">';
+            else
+                echo '<button type = "'.$value[1].'" name = "'.$value[2].'" value = "'.$value[2].'" class = "'.$value[3].'">'.$value[4].'</button>';
+        }
+        echo '</form>';
+    }
+    
+    public function setMethod($value){
+        if (isset($value))
+            $this->_method = $value;
+    }
+    
+    public function setAction($value){
+        if (isset($value))
+            $this->_action = $value;
+    }
+    
+    public function setClass($value){
+        if (isset($value))
+            $this->_class = $value;
+    }
+    
+    public function setId($value){
+        if (isset($value))
+            $this->_id = $value;
+    }
+
+    public function setTitle($value){
+        if (isset($value))
+            $this->_title = $value;
+    }
+    
+    public function getMethod(){
+        return ($this->_method);
+    }
+    
+    public function getAction(){
+        return ($this->_action);
+    }
+    
+    public function getClass(){
+        return ($this->_class);
+    }
+    
+    public function getId(){
+        return ($this->_id);
+    }
+
+    public function getTitle(){
+        return ($this->_title);
+    }
+}
+?>
--- htdocs/profile.php
+++ htdocs/profile.php
@@ -10,6 +10,10 @@ if (isset($_SESSION['id'])){
         $deleteAc = new DeleteAccount($_SESSION['id'], $_POST['pass'], $db);
         $message = $deleteAc->eraseAccount();
     }
+    if (isset($_POST['submit'] && $_POST['submit'] === "modif_pass")){
+        $newPass = new ModifPass($_SESSION['id'], $_POST['old_pass'], $_POST['new_pass'], $_POST['new_pass_verif']);
+        $message = $newPass->getMessage();
+    }
 }
 if (isset($_SESSION['id'])){
     ?>
@@ -25,9 +29,13 @@ if (isset($_SESSION['id'])){
         echo '<h2>Profile de '.$_SESSION['login'].'</h2><img class = "profile-picture" alt = "'.$_SESSION['name'].'" src = "'.$_SESSION['url'].'"><p>'.$_SESSION['mail'].'</p><p>Type de compte : '.$_SESSION['type'].'</p><p>Role : '.$_SESSION['role'].'</p><p>Inscription : Le '.$_SESSION['ins_day'].'/'.$_SESSION['ins_month'].'/'.$_SESSION['ins_year'].' à '.$_SESSION['ins_hour'].':'.$_SESSION['ins_min'].':'.$_SESSION['ins_sec'].'</p>';
         if (isset($message))
             echo '<p class = "'.$message[1].'">'.$message[0].'</p>';
+    
+        //Appel de la classe StdForm pour créer un formulaire standard
+            $modif_pass = new StdForm("post", "profile.php", "standard-form regular-form", "modif_pass", "Modification du mot de passe");
+            $modif_pass->addInputs(array(array("input", "password", "old_pass", "field", "Ancien mot de passe"), array("input", "password", "new_pass", "field", "Nouveau mot de passe"), array("input", "password", "new_pass_verif", "field", "Vérificaiton du mot de passe"), array("button" ,"submit", "modif_pass", "btn-form", "Modifier le mot de passe")));
         ?>
         <form class = "delete-form" action = "profile.php" method = "post">
-        <p>Suppression du compte</p>
+        <h3>Suppression du compte</h3>
         <input class = "field" type = "password" name = "pass" placeholder = "Votre mot de passe...">
         <button class = "btn-form btn-delete" name = "submit" value = "delete_account">Supprimer mon compte</button>
         </form>
